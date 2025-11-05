bc26a4efba8fb5464f470ea86cbe2d120b76e627
Himanshu Kumar Gupta <hkgupta@microsoft.com>
2025-11-05 15:06:33 +0530

    Cleanups

--- Sxg.EvalPlatform.API.Storage/Entities/SelectedMetricsConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sxg.EvalPlatform.API.Storage.Entities
-{
-    public class SelectedMetricsConfiguration
-    {
-        public string MetricName { get; set; }
-        public double Threshold { get; set; }
-    }
-}
